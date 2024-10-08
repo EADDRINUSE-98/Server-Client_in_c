96772a776542e15334902f46501998a083ac07c1
PriyanshuSingh <singhpriyanshu7827@gmail.com>
2024-10-08 22:11:53 +0530

    Put signal action funtionality.

--- tcpserver.c
+++ tcpserver.c
@@ -1,30 +1,38 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<string.h>
-#include<errno.h>
-#include<sys/socket.h>
-#include<sys/types.h>
-#include<netdb.h>
+#include<stdio.h> // perror(), fprintf(), 
+#include<stdlib.h> // exit(),
+#include<unistd.h> // close(),
+#include<string.h> // strerror(), memset()
+#include<errno.h> // errno,
+#include<sys/socket.h> // AF_UNSPEC, AI_PASSIVE, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR, EXIT_FAILURE, EXIT_SUCCESS, getaddrinfo(), gai_strerror(), socket(), setsockopt(), bind(), freeaddrinfo(), listen(),
+#include<sys/types.h> // getaddrinfo(), gai_strerror(), socket(), setsockopt(), bind(), freeaddrinfo(), listen(),
+#include<netdb.h> // getaddrinfo(), gai_strerror(), freeaddrinfo(),
 #include<arpa/inet.h>
 #include<netinet/in.h>
 
+#define BACKLOG 5
 #define SERVPORT "6789"
+
+void signal_handler_func(int signum){
+	int saved_errno = errno;
+	while((waitpid(-1, NULL, WOHANG)) > 0);
+	errno = saved_errno;
+}
 
 int main(){
 	int sockfd;
-	int rv;
+	int status;
 	struct addrinfo hints, *servinfo, *p;
 	int numbytes;
 	int yes = 1;
+	struct sigaction sa;
 
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_flags = AI_PASSIVE;
 	hints.ai_socktype = SOCK_STREAM;
 
-	if((rv = getaddrinfo(NULL, SERVPORT, &hints, &servinfo)) != 0){
-		fprintf(strerr, "getaddinfo: %s\n", gai_strerror(rv));
+	if((status = getaddrinfo(NULL, SERVPORT, &hints, &servinfo)) != 0){
+		fprintf(strerr, "getaddinfo: %s\n", gai_strerror(status));
 		exit(EXIT_FAILURE);
 	}
 
@@ -35,6 +43,7 @@ int main(){
 		}
 		if((setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int))) != 0){
 			perror("server: setsockopt\n");
+			fprintf(strerr, "error msg: %s\n", strerror(errno));
 			exit(EXIT_FAILURE);
 		}
 		if((bind(sockfd, p->ai_addr, p->ai_addrlen)) != 0){
@@ -43,5 +52,29 @@ int main(){
 			continue;
 		}
 		break;
+	}
+
+	freeaddrinfo(servinfo);
+
+	if(p == NULL){
+		fprintf(strerr, "server: failed to bind\n");
+		fprintf(strerr, "error msg: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	if((listen(sockfd, BACKLOG)) != 0){
+		perror("server: listen\n");
+		fprintf(strerr, "error msg: %s\n", strerror(errno));
+		close(sockfd);
+		exit(EXIT_FAILURE);
+	}
+
+	sa.sa_handler = signal_handler_func;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	if((sigaction(SIGCHLD, &sa, NULL)) != 0){
+		perror("server: sigaction\n");
+		fprintf(strerr, "error msg: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
 	}
 }
